e90284f01e4c6fc7a04199e754dc1b05cf0b9c65
Petter Amaleona <ravenda900@gmail.com>
2020-07-03 02:17:35 +0800

    Modified table data

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -28,61 +28,25 @@
                                     tbody-classes="list"
                                     :data="tableData">
                           <template slot="columns">
-                            <th>Project</th>
-                            <th>Budget</th>
-                            <th>Status</th>
-                            <th>Users</th>
-                            <th>Completion</th>
+                            <th>Order Id</th>
+                            <th>Order Date</th>
+                            <th>Order Name</th>
+                            <th>Customer Name</th>
                             <th></th>
                           </template>
 
                           <template slot-scope="{row}">
                             <th scope="row">
-                              <div class="media align-items-center">
-                                <a href="#" class="avatar rounded-circle mr-3">
-                                  <img alt="Image placeholder" :src="row.img">
-                                </a>
-                                <div class="media-body">
-                                  <span class="name mb-0 text-sm">{{row.title}}</span>
-                                </div>
-                              </div>
+                              {{row.orderId}}
                             </th>
-                            <td class="budget">
-                              {{row.budget}}
+                            <td>
+                              {{row.orderDate}}
                             </td>
                             <td>
-                              <badge class="badge-dot mr-4" :type="row.statusType">
-                                <i :class="`bg-${row.statusType}`"></i>
-                                <span class="status">{{row.status}}</span>
-                              </badge>
+                              {{row.orderName}}
                             </td>
                             <td>
-                              <div class="avatar-group">
-                                <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Ryan Tompson">
-                                  <img alt="Image placeholder" src="img/theme/team-1-800x800.jpg">
-                                </a>
-                                <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Romina Hadid">
-                                  <img alt="Image placeholder" src="img/theme/team-2-800x800.jpg">
-                                </a>
-                                <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Alexander Smith">
-                                  <img alt="Image placeholder" src="img/theme/team-3-800x800.jpg">
-                                </a>
-                                <a href="#" class="avatar avatar-sm rounded-circle" data-toggle="tooltip" data-original-title="Jessica Doe">
-                                  <img alt="Image placeholder" src="img/theme/team-4-800x800.jpg">
-                                </a>
-                              </div>
-                            </td>
-
-                            <td>
-                              <div class="d-flex align-items-center">
-                                <span class="completion mr-2">{{row.completion}}%</span>
-                                <div>
-                                  <base-progress :type="row.statusType"
-                                                 :show-percentage="false"
-                                                 class="pt-0"
-                                                 :value="row.completion"/>
-                                </div>
-                              </div>
+                              {{row.customerName}}
                             </td>
 
                             <td class="text-right">
@@ -118,52 +82,24 @@
     </div>
 </template>
 <script>
+  import faker from 'faker';
+
   export default {
     name: 'orders',
     data() {
+      const tableData = [];
+
+      for (let i = 0 ; i < faker.random.number() ; i++) {
+        tableData.push({
+          orderId: faker.random.number(),
+          orderDate: faker.date.future(),
+          orderName: faker.commerce.productName(),
+          customerName: faker.name.findName()
+        })
+      }
+
       return {
-        tableData: [
-          {
-            img: 'img/theme/bootstrap.jpg',
-            title: 'Argon Design System',
-            budget: '$2500 USD',
-            status: 'pending',
-            statusType: 'warning',
-            completion: 60
-          },
-          {
-            img: 'img/theme/angular.jpg',
-            title: 'Angular Now UI Kit PRO',
-            budget: '$1800 USD',
-            status: 'completed',
-            statusType: 'success',
-            completion: 100
-          },
-          {
-            img: 'img/theme/sketch.jpg',
-            title: 'Black Dashboard',
-            budget: '$3150 USD',
-            status: 'delayed',
-            statusType: 'danger',
-            completion: 72
-          },
-          {
-            img: 'img/theme/react.jpg',
-            title: 'React Material Dashboard',
-            budget: '$4400 USD',
-            status: 'on schedule',
-            statusType: 'info',
-            completion: 90
-          },
-          {
-            img: 'img/theme/vue.jpg',
-            title: 'Vue Paper UI Kit PRO',
-            budget: '$2200 USD',
-            status: 'completed',
-            statusType: 'success',
-            completion: 100
-          }
-        ]
+        tableData: tableData
       }
     }
   };
